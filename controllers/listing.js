4508402f80f0aef19a4e554e2a9caa3804c0998d
James Palumbo <jamesmpalumbo1@gmail.com>
2025-04-08 20:03:47 -0400

    created listing controller/ licked to server.js/ added boilerplate to listing controller

---
+++ controllers/listing.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const verifyToken = require("../middleware/verify-token.js");
+const router = express.Router();
+const Listing = require("../models/listing.js");
+
+//I.N.D.U.C.E.S.
+
+// Index
+
+// Delete
+
+// Update
+
+// Create
+
+// Show
+
+module.export = router;
