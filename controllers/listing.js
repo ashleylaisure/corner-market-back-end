b81ee457cd71b9d9044353957cd384c0cddee462
James Palumbo <jamesmpalumbo1@gmail.com>
2025-04-08 20:42:24 -0400

    fixed typo and added error handling

--- controllers/listing.js
+++ controllers/listing.js
@@ -16,9 +16,14 @@ const Listing = require("../models/listing.js");
 // Show Listing details
 router.get("/:listingId", async (req, res) => {
   try {
-    const listing = await Listing.findbyId(req.params.listingId).populate([
+    const listing = await Listing.findById(req.params.listingId).populate([
       "author",
     ]);
+
+    if (!listing) {
+      return res.status(404).json({ error: "Listing not found" });
+    }
+
     res.status(200).json(listing);
   } catch (err) {
     res.status(500).json({ err: err.message });
