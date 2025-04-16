4e3d682ed057c09774b2466ce5ab1cedf1aba91d
James Palumbo <jamesmpalumbo1@gmail.com>
2025-04-16 11:02:28 -0400

    updated profile model to include default fields/ updated signup route to create profile upon user creation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,51 +1,70 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const User = require('../models/user');
+const User = require("../models/user");
+const Profile = require("../models/profile");
 
 const saltRounds = 12;
 
-router.post('/sign-up', async (req, res) => {
+router.post("/sign-up", async (req, res) => {
   try {
     // const userInDatabase = await User.findOne({ username: req.body.username });
 
     // making the search case insestive
-    const userInDatabase = await User.findOne({ 
-      username: { $regex: new RegExp(`${req.body.username}$`, 'i') }})
-    
+    const userInDatabase = await User.findOne({
+      username: { $regex: new RegExp(`${req.body.username}$`, "i") },
+    });
+
     if (userInDatabase) {
-      return res.status(409).json({err: 'Username already taken.'});
+      return res.status(409).json({ err: "Username already taken." });
     }
-    
+
     const user = await User.create({
       username: req.body.username,
-      hashedPassword: bcrypt.hashSync(req.body.password, saltRounds)
+      hashedPassword: bcrypt.hashSync(req.body.password, saltRounds),
     });
+
+    const profile = await Profile.create({
+      user: user._id,
+      memberSince: Date.now(),
+    });
+    const savedProfile = await profile.save();
+
+    user.profile = savedProfile._id;
+    await user.save();
 
     const payload = { username: user.username, _id: user._id };
 
     const token = jwt.sign({ payload }, process.env.JWT_SECRET);
 
-    res.status(201).json({ token });
+    res.status(201).json({
+      token,
+      user: {
+        _id: user._id,
+        username: user.username,
+        profile: savedProfile,
+      },
+    });
   } catch (err) {
     res.status(500).json({ err: err.message });
   }
 });
 
-router.post('/sign-in', async (req, res) => {
+router.post("/sign-in", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
     if (!user) {
-      return res.status(401).json({ err: 'Invalid credentials.' });
+      return res.status(401).json({ err: "Invalid credentials." });
     }
 
     const isPasswordCorrect = bcrypt.compareSync(
-      req.body.password, user.hashedPassword
+      req.body.password,
+      user.hashedPassword
     );
     if (!isPasswordCorrect) {
-      return res.status(401).json({ err: 'Invalid credentials.' });
+      return res.status(401).json({ err: "Invalid credentials." });
     }
 
     const payload = { username: user.username, _id: user._id };
